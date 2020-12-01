10f615380a725b7a5e00257523dcf04fc78484b7
lykhachov <ilyalykhachov.com.ua>
2020-12-01 11:53:26 +0200

    static tests

---
+++ dev/tests/static/testsuite/Magento/Test/Legacy/_files/obsolete_methods_storefront_search_ce.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Obsolete methods
+ * Format: array(<method_name = ''>[, <class_scope> = ''[, <replacement>[, <is_deprecated>]]])
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+// @codingStandardsIgnoreFile
+
+return [
+    ['getFacets', 'Magento/SearchStorefrontApi/Api/Data/ProductsSearchResultInterface']
+];
